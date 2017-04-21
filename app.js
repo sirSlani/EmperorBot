03fc2306e1d03e36ef68f513d2b8f3f1ca0021b7
Matija Maric <sirSlani@gmail.com>
2017-04-21 20:31:06 +0200

    Added Discord.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('Bot ready');
+});
+
+client.login('MzAzMjAxNDQwODUwMjQ3Njgx.C9Up5g.cRdYcZWVOBVn1n-lIPiipnKrEx8');
