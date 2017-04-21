9b38e58ca2b1e49d9f28a5ccd96d5c0c38c1b023
Matija Maric <sirSlani@gmail.com>
2017-04-21 20:44:31 +0200

    Ping

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const settings = require('./settings.json');
 
 const client = new Discord.Client();
 
+let prefix = "$";
+
 client.on('ready', () => {
   console.log('Bot ready');
 });
+
+client.on('message', (message) => {
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.sendMessage("Fuck off.");
+  }
+})
 
 client.login(settings.discordToken);
